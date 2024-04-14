d9e52379418269d27f6712fdea54dbd69e7a09e4
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-04-14 15:14:59 +0800

    change the solution to 2 decimal places

--- src/Circle.java
+++ src/Circle.java
@@ -1,16 +1,16 @@
 public class Circle extends Shape {
 
-    private double radius;
+    private final double radius;
 
     Circle(double radius) {
         this.radius = radius;
     }
 
     public double calculateArea() {
-        return Math.round(Math.PI * radius * radius) ;
+        return(double) Math.round((Math.PI * radius * radius)*100)/100;
     }
 
     public double calculatePerimeter() {
-        return Math.round( 2 * Math.PI * radius);
+        return(double) Math.round((2 * Math.PI * radius)*100)/100;
     }
 }
